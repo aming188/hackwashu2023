9bca097cd0b8aa27261903c8b3f807f72a23ba32
albert1003-web <albertming88@gmail.com>
2023-10-14 10:48:15 -0500

    next.js initialization

---
+++ prisma/migrations/20231014154155_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL
+);
